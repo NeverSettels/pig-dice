9e42d032f786740c6cb7a82693c033eba41d2921
Eric Settels and Jonathan Carlos <eric@>
2020-01-30 12:21:34 -0800

    add roll button to html, game and player function to JavaScript, random number generator and set score function to Javascript.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,46 @@
-$(document).ready(function() {
-  $("#names").submit(function(event) {
-    event.preventDefault();
-    var playerOne = ("#playerOne").val();
-    var playerTwo = ("#playerTwo").val();
-  });
+function Game () {
+  this.playerOne = {};
+  this.playerTwo = {};
+  this.dice = 0;
 }
+
+function Player (name) {
+  this.name = name;
+  this.totalScore = 0;
+  this.roundScore = 0;
+}
+
+Player.prototype.getDice = function () {
+  return Math.floor(Math.random() * (6)) + 1;
+}
+Player.prototype.setScore = function(){
+  var roll = this.getDice()
+  console.log(roll)
+  if(roll === 1){
+    this.roundScore = 0
+  }else {
+    return this.roundScore += roll;
+  }
+}
+
+$(document).ready(function() {
+  var newGame = new Game ();
+  console.log(newGame)
+  $("#nameInput").submit(function(event) {
+    event.preventDefault();
+    newGame.playerOne = new Player($("#playerOne").val());
+    console.log(newGame)
+    newGame.playerTwo = new Player($("#playerTwo").val());
+    $('#roll').removeClass('no-display')
+    
+    
+  });
+  $("#roll").submit(function(event) {
+    event.preventDefault();
+    newGame.playerOne.setScore();
+    console.log(newGame)
+    
+  })
+
+
+})
