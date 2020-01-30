3f30d342a91819412f36a172100acce5cddcbc45
Eric Settels and Jonathan Carlos <jonathan@>
2020-01-29 16:25:38 -0800

    add form in html and player one and two var in JavaScript.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
 $(document).ready(function) {
-  
+  $("#names").submit(function(event) {
+    event.preventDefault();
+    var playerOne = ("#playerOne").val();
+    var playerTwo = ("#playerTwo").val();
+  });
 }
